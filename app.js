01b355850b9c8d4248ae1788e16f6cd1d0fd2bc0
fchung7 <58450122+fchung7@users.noreply.github.com>
2020-07-15 00:03:59 -0400

    Add motivational timer files

---
+++ app.js
@@ -0,0 +1,76 @@
+const startingCount = 25;
+let timeInSeconds = startingCount * 60;
+let intervalCount;
+
+const countStart = document.querySelector(".count-down");
+const buttonStart = document.querySelector(".start");
+const buttonReset = document.querySelector(".reset");
+
+const body = document.querySelector("body")
+const buttonMode = document.querySelector(".mode");
+const container = document.querySelector(".container")
+
+buttonMode.addEventListener("click", function(){
+    body.classList.toggle("dark")
+    container.classList.toggle("dark")
+    buttonMode.classList.toggle("btn-dark")
+    buttonMode.classList.toggle("btn-light")
+    buttonStart.classList.toggle("btn-light")
+    buttonStart.classList.toggle("btn-dark")
+    buttonReset.classList.toggle("btn-light")
+    buttonReset.classList.toggle("btn-dark")
+})
+
+buttonStart.addEventListener("click", start);
+
+function start() {
+  intervalCount = setInterval(updateCountDown, 1000);
+
+  function updateCountDown() {
+    let minutes = Math.floor(timeInSeconds / 60);
+    let seconds = timeInSeconds % 60;
+    //If there is still time left, decrement timeInSeconds.
+
+    if (timeInSeconds > 0) {
+      timeInSeconds--;
+    } else {
+      //When there is no time left, stop timer.
+      clearInterval(intervalCount);
+    }
+
+    if (seconds < 10) {
+      countStart.innerHTML = minutes + ":0" + seconds;
+    } else {
+      countStart.innerHTML = minutes + ":" + seconds;
+    }
+    //Show time on browser Tab
+    document.title = "Motivational Timer " + countStart.innerHTML;
+
+    if (seconds === 0 && minutes === 0) {
+      let audio = new Audio("vitinho2.mp3");
+      audio.play();
+    }
+    console.log(seconds);
+  }
+  console.log("start");
+  buttonStart.removeEventListener("click", start);
+  buttonStart.innerHTML = "Pause";
+  buttonStart.addEventListener("click", pause);
+}
+
+function pause() {
+  console.log("pause");
+  buttonStart.removeEventListener("click", pause);
+  clearInterval(intervalCount);
+  buttonStart.innerHTML = "Start";
+  buttonStart.addEventListener("click", start);
+}
+
+buttonReset.addEventListener("click", function () {
+  timeInSeconds = startingCount * 60;
+  clearInterval(intervalCount);
+  buttonStart.innerHTML = "Start";
+  buttonStart.addEventListener("click", start);
+  countStart.innerHTML = "25:00";
+  document.title = "Motivational Timer " + countStart.innerHTML;
+});
